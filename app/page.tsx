228846b9946161169d357d7f15e4ec263957b002
Vladyslav Dalechyn <vlad.dalechin@gmail.com>
2024-04-12 20:22:35 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,22 @@ export default function Home() {
   return (
     <div className="prose dark:prose-invert">
       <div className="flex flex-col items-center">
-        <h1 className="mb-2">hotwater</h1>
+        <h1 className="mb-2">Vladyslav Dalechyn (dalechyn.eth)</h1>
         <h4 className="mt-4 text-center">full stack engineer building performant and high-assurance applications</h4>
-        <h6 className="text-center">contact me via <a href="mailto:vladyslav@spilnota.xyz">mail</a> or view my <a href="https://github.com/dalechyn">github</a> or <a href="https://twitter.com/dalechyn">twitter</a></h6>
+        <h6 className="text-center">contact me via <a href="mailto:vladyslav@spilnota.xyz">mail</a> or view my <a href="https://github.com/dalechyn">github</a> or <a href="https://twitter.com/dalechyn">twitter</a> or <a href="https://warpcast.com/dalechyn.eth">warpcast</a></h6>
       </div>
       <hr/>
       <h1>Open Source Work</h1>
       <ul>
-        <li><a className="font-mono" href="https://github.com/spilnotaxyz/chyn">spilnotaxyz/chyn</a> – a headless web3 ui component library built with shadcn and wagmi;</li>
-        <li><a className="font-mono" href="https://github.com/AbstractSDK/abstract.js">abstractSDK/abstract.js</a> – a typescript sdk for interacting with Abstract CosmWasm contracts and generating contract-specific React/Node.js code;</li>
-        <li><a className="font-mono" href="https://github.com/valorem-labs-inc/typescript-sdk">valorem-labs-inc/typescript-sdk</a> – a typescript sdk for interacting with valorem&apos;s <a className="font-mono" href="https://github.com/valorem-labs-inc/clear">clear</a> protocol and <a className="font-mono" href="https://github.com/valorem-labs-inc/trade-interfaces">trade-api</a>;</li>
-        <li><a className="font-mono" href="https://github.com/valorem-labs-inc/clear-periphery">valorem-labs-inc/clear-periphery</a> – a cash settlement periphery contract using <a className="font-mono" href="https://github.com/Uniswap/v3-core">uniswap-v3</a> flash loans for exercising options in <a className="font-mono" href="https://github.com/valorem-labs-inc/clear">clear</a> protocol;</li>
-        <li><a className="font-mono" href="https://github.com/valorem-labs-inc/trade-interfaces">valorem-labs-inc/trade-interfaces</a> – a trade api enabling peer-to-peer, signature-based, noncustodial digital asset trading via low latency <a href="https://grpc.io/docs/what-is-grpc/introduction/">grpc</a> and <a href="https://github.com/grpc/grpc-web">grpc-web</a> tls-encrypted version 3 protocol buffer interfaces, with order settlement via <a className="font-mono" href="https://github.com/ProjectOpenSea/seaport">seaport</a> protocol;</li>
-        <li><a className="font-mono" href="https://github.com/paradigmxyz/flux">paradigmxyz/flux</a> – a graph-based LLM power tool for exploring many completions in parallel;</li>
-        <li><a className="font-mono" href="https://github.com/h0tw4t3r/arbitrageur">h0tw4t3r/arbitrageur</a> – an attempt to extract cross-dex mev amongst <a className="font-mono" href="https://github.com/Uniswap/v2-core">uniswap-v2</a> and <a className="font-mono" href="https://github.com/Uniswap/v3-core">uniswap-v3</a> forks;</li>
-        <li><a className="font-mono" href="https://github.com/h0tw4t3r/abi-signature-bruteforcer">h0tw4t3r/abi-signature-bruteforcer</a> – a cpu-based solidity function signature bruteforcer to save extra gas on calls;</li>
-        <li><a className="font-mono" href="https://github.com/rainbow-me/rainbowkit">rainbow-me/rainbowkit</a> – a react library that makes it easy to add wallet connection to your dapp;</li>
-        <li><a className="font-mono" href="https://github.com/heyxyz/hey">heyxyz/hey</a> – a decentralized and permissionless social media app built with lens protocol;</li>
-        <li><a className="font-mono" href="https://github.com/attestate/ERC4973">attestate/ERC4973</a> – a reference implementation of eip-4973 &quot;account-bound tokens&quot;</li>
+        <li><a className="font-mono" href="https://github.com/wevm/frog">wevm/frog</a> – framework for Farcaster Frames. Actively collaborating.</li>
+        <li><a className="font-mono" href="https://github.com/spilnotaxyz/chyn">spilnotaxyz/chyn</a> – a headless web3 ui component library built with shadcn and wagmi. An open-source experiment.</li>
+        <li><a className="font-mono" href="https://github.com/AbstractSDK/abstract.js">abstractSDK/abstract.js</a> – a typescript sdk for interacting with Abstract CosmWasm contracts and generating contract-specific React/Node.js code. Author.</li>
+        <li><a className="font-mono" href="https://github.com/valorem-labs-inc/typescript-sdk">valorem-labs-inc/typescript-sdk</a> – a typescript sdk for interacting with valorem&apos;s <a className="font-mono" href="https://github.com/valorem-labs-inc/clear">clear</a> protocol and <a className="font-mono" href="https://github.com/valorem-labs-inc/trade-interfaces">trade-api</a>; Collaborated in past.</li>
+        <li><a className="font-mono" href="https://github.com/valorem-labs-inc/clear-periphery">valorem-labs-inc/clear-periphery</a> – a cash settlement periphery contract using <a className="font-mono" href="https://github.com/Uniswap/v3-core">uniswap-v3</a> flash loans for exercising options in <a className="font-mono" href="https://github.com/valorem-labs-inc/clear">clear</a> protocol; Collaborated in past.</li>
+        <li><a className="font-mono" href="https://github.com/valorem-labs-inc/trade-interfaces">valorem-labs-inc/trade-interfaces</a> – a trade api enabling peer-to-peer, signature-based, noncustodial digital asset trading via low latency <a href="https://grpc.io/docs/what-is-grpc/introduction/">grpc</a> and <a href="https://github.com/grpc/grpc-web">grpc-web</a> tls-encrypted version 3 protocol buffer interfaces, with order settlement via <a className="font-mono" href="https://github.com/ProjectOpenSea/seaport">seaport</a> protocol; Collaborated in past.</li>
+        <li><a className="font-mono" href="https://github.com/paradigmxyz/flux">paradigmxyz/flux</a> – a graph-based LLM power tool for exploring many completions in parallel; Contributed with ideas.</li>
+        <li><a className="font-mono" href="https://github.com/h0tw4t3r/arbitrageur">h0tw4t3r/arbitrageur</a> – an attempt to extract cross-dex mev amongst <a className="font-mono" href="https://github.com/Uniswap/v2-core">uniswap-v2</a> and <a className="font-mono" href="https://github.com/Uniswap/v3-core">uniswap-v3</a> forks; My failed attempt to become MEV searcher.</li>
+        <li><a className="font-mono" href="https://github.com/h0tw4t3r/abi-signature-bruteforcer">h0tw4t3r/abi-signature-bruteforcer</a> – a cpu-based solidity function signature bruteforcer to save extra gas on calls; Author.</li>
         <li><a className="font-mono" href="https://github.com/spilnotaxyz/nfting-frontend">spilnotaxyz/nfting-frontend</a> & <a className="font-mono" href="https://github.com/spilnotaxyz/nfting-backend">spilnotaxyz/nfting-backend</a>  – a web application that gathers ethereum account data and outputs nft statistics;</li>
       </ul>
       <hr/>
